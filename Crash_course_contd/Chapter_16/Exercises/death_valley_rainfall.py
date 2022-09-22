2ffd8f26703de1f96c45a168e6814355264fe4d1
Edward Eleazar <laizer2100@gmail.com>
2022-09-22 09:32:53 +0100

    Added Chap 16,Exercise1 : Rainfall in Sitka,Alaska & Death Valley,CA

---
+++ Crash_course_contd/Chapter_16/Exercises/death_valley_rainfall.py
@@ -0,0 +1,36 @@
+# Exercise 16.1 : Sitka Rainfall
+import csv
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+filename = 'data/death_valley_2018_simple.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    # for index,c_header in enumerate(header_row):
+    #     print(c_header)
+
+    #Generating data
+    dates = []
+    prcps = []
+    for row in reader:
+        current_date =  datetime.strptime(row[2], '%Y-%m-%d')   
+        current_prcp = float(row[3])  
+        dates.append(current_date)
+        prcps.append(current_prcp)
+
+#Plotting the rainfall chart
+plt.style.use('seaborn')
+fig,ax = plt.subplots()
+ax.plot(dates,prcps, c='red', alpha=0.5)
+
+#Format plot
+ax.set_title('Daily rainfall in Death Valley, CA \n 2018', fontsize=20)
+ax.set_xlabel('Dates',fontsize=10)
+fig.autofmt_xdate()
+ax.set_ylabel('Precipitation', fontsize=10)
+ax.tick_params(axis='both', which='major', labelsize=10)
+
+plt.show()
+# plt.savefig('death_valley_rainfall.png',bbox_inches ='tight')         
